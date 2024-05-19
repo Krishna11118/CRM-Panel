807535129a65850522ce96ae1999c4876899d738
Krishna . <krishnassss365@gmail.com>
2024-05-19 18:56:52 +0000

    env

--- client/src/Config/Config.js
+++ client/src/Config/Config.js
@@ -1,5 +1,6 @@
 const config = {
-  endpoint: `http://localhost:3001/v1`,
+  // endpoint: `http://localhost:3001/v1`,
+  endpoint: `https://3001-krishna1111-crmadminmai-47vmgb42ky2.ws-us110.gitpod.io/v1`,
 };
 
 export default config;
