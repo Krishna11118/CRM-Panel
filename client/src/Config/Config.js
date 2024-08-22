0f42f013a09338e7133ab03ce5a0b6bad3873ca6
Krishna . <krishnassss365@gmail.com>
2024-08-22 10:31:24 +0000

    configAdded

--- client/src/Config/Config.js
+++ client/src/Config/Config.js
@@ -1,6 +1,11 @@
 const config = {
   // endpoint: `http://localhost:3001/v1`,
-  endpoint: `https://crm-admin-main.onrender.com/v1`,
+  
+  // vercel
+  endpoint: `https://crm-admin-main-s726.vercel.app/v1`
+
+  //working render
+  // endpoint: `https://crm-admin-main.onrender.com/v1`,
 };
 
 export default config;
