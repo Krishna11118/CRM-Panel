44666dd884a18ef0e642769ddc5ce222be8aa465
Krishna <61353958+Krishna11118@users.noreply.github.com>
2024-08-27 20:55:56 +0530

    Update config.js

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,11 +1,9 @@
 const config = {
   // endpoint: `http://localhost:3001/v1`,
   
-  // vercel
+  // vercel deployed backend
   endpoint: `https://crm-admin-main-s726.vercel.app/v1`
 
-  //working render
-  // endpoint: `https://crm-admin-main.onrender.com/v1`,
 };
 
 export default config;
