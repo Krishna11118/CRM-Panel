b853e2401d869e915df3fbbc8ec9ad33bb04b1e8
krishna11118 <krishnassss@gmail.com>
2024-12-04 01:13:46 +0530

    Addedserverurl

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,8 +1,8 @@
 const config = {
-  endpoint: `http://localhost:3001/v1`,
+  // endpoint: `http://localhost:3001/v1`,
   
   // vercel deployed backend
-  // endpoint: `https://crm-admin-main-s726.vercel.app/v1`
+  endpoint: `https://crm-admin-main-s726.vercel.app/v1`
 
 };
 
